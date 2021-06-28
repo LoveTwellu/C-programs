e96988bc9c2aaeeb8f0134427761bdc05734fdbe
Sikandar.S.Bhide <aptebaburao08@gmail.com>
2021-06-28 23:35:05 +0530

    C ++ Program

---
+++ Basic.cpp
@@ -0,0 +1,12 @@
+#include <iostream> //<iostream> <-- Known as header file
+// iostream name ke file pahile see mojud hai c mai
+// iosream help in Input & Output ((I-input O-Output ))
+int main()
+{
+    // int- Integer, main-Function int main is entry point of all code ,code is going to lye in  int main ,So like this there are many functions each function  has it's own specfic value
+        std::cout << "Hello World \n";
+    //  This is instruction end with Semicolon (;)
+    //   std is standard
+
+    return 0;
+}
---
+++ Operator.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+
+    int main()
+    {       
+        // int a, b;
+        int a;
+        float b;
+        // a = 20;
+        // b = 50;
+        printf("Enter Value of a : ",a);
+        scanf("%d",&a);
+        printf("Enter Value of b : ",b);
+        scanf("%f",&b);
+        // printf("Hello World!\n"); 
+        printf("The Addition or u can say a+b = %f\n",a+b);
+        printf("The Subtraction or u can say a-b = %f\n",a-b);
+        printf("The Multipe or u can say a*b = %f\n",a*b);
+        return 0;
+    }
+    
+
+
