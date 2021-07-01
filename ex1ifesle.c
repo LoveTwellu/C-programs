220a075d621a8d8b057298ae4e0cfb47d45a1b36
Sikandar.S.Bhide <aptebaburao08@gmail.com>
2021-07-02 02:37:58 +0530

    if else Even or Odd

---
+++ ex1ifesle.c
@@ -0,0 +1,20 @@
+// Check whether a given number is even or odd
+#include <stdio.h>
+int main()
+{
+    int a ;
+    printf("Enter Number : ");
+    scanf("%d",&a);
+    if (a%2 == 0)
+    {
+        printf("it's Even Number \n");
+    }
+    else
+    {
+        printf("It's odd number\n");
+    }
+    
+    
+    return 0;
+}
+
